517245213b0fa7f2265daa42727e00a3c31fbe78
yihong <swes101132@gmail.com>
2022-09-13 19:59:22 +0800

    commit 2

---
+++ config.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/flask2"
+SQLALCHEMY_TRACK_MODIFICATIONS = "false"
--- hello.py
+++ hello.py
@@ -1,12 +1,42 @@
 from flask import Flask
-
-from flask import render_template
+from flask import render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config.from_object("config")
+db = SQLAlchemy(app)
 
-@app.route("/")
+class User2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+
+
+@app.route("/", methods=["GET","POST"])
 def hello_world():
+    if(request.method == 'POST'):
+        # save data
+        username = request.form["username"]
+        email = request.form["email"]
+        user = User2(id=1,username=username,email=email)
+        db.session.add(user)
+        db.session.commit()
+        return "Updated"
 
-    return render_template("test.html")
+    else:
+        return render_template("test.html")
 
+@app.route("/hello")
+def hello():
+    return "hello"
+
+@app.route("/hello/<username>")
+def hello_user(username):
+    return "hello" + " " + username + url_for("hello")
+
+@app.route("/hello/<int:x>")
+def x(x):
+    a = 2+x
+    return "2+" + str(x) + "=" + str(a)
