b2f59c4b4557cb702ad965e078fb069a28563134
yihong <swes101132@gmail.com>
2022-09-13 13:57:37 +0800

    commit 2

--- hello.py
+++ hello.py
@@ -1,15 +1,12 @@
 from flask import Flask
-<<<<<<< HEAD
-=======
+
 from flask import render_template
->>>>>>> 9087ba6 (Initial commit)
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-<<<<<<< HEAD
-    return "<p>Hello, World!</p>"
-=======
+
     return render_template("test.html")
->>>>>>> 9087ba6 (Initial commit)
+
